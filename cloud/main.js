3bec431baeb6b070ddd14d8bc3657f7b3f8447dc
Shubham Kansal <kansalshubham46@gmail.com>
2015-06-09 21:17:24 +0530

    version 1.10
    Added background job for sending periodic like notifications

--- cloud/main.js
+++ cloud/main.js
@@ -16,6 +16,7 @@ var login = require('cloud/login.js');
 var mail = require('cloud/mail.js');
 var analytics = require('cloud/analytics.js');
 var followup = require('cloud/followup.js');
+var _ = require('underscore.js');
 
 /*------------------------------------------------after/before functions---------------------------*/
 Parse.Cloud.afterSave("Messageneeders", function(request) {
@@ -183,6 +184,47 @@ Parse.Cloud.job("userMigration", function(request, status) {
         status.error("Uh oh, something went wrong.");
     });
 });
+
+Parse.Cloud.job("sendLikeNotifications", function(request, status){ 
+    var intervals = 4; 
+    var intervalTime = (86400000/ intervals);
+    var date = new Date();
+    var currentTime = date.getTime();
+    var dateLowerBound = new Date(currentTime - intervals * intervalTime);
+    var dateUpperBound = new Date(currentTime - (intervals - 1) * intervalTime);
+    var query1 = new Parse.Query("GroupDetails");
+    query1.greaterThanOrEqualTo("createdAt", dateLowerBound);
+    query1.lessThan("createdAt", dateUpperBound);
+    query1.greaterThanOrEqualTo("like_count", 0);
+    query1.select("objectId", "name", "title", "like_count", "senderId");
+    query1.find().then(function(results){
+        var promise = Parse.Promise.as();
+        Parse.Cloud.useMasterKey();
+        _.each(results, function(result){
+            var query2 = new Parse.Query(Parse.Installation);
+            query2.equalTo("username", result.get("senderId")); 
+            promise = promise.then(function(){
+                return Parse.Push.send({
+                    where: query2,
+                    data: {
+                        msg: "Hey !:)",
+                        alert: "Hey! :)",
+                        badge: "Increment",
+                        groupName: "classname",
+                        type: "NORMAL",
+                action: "INBOX"
+                    }
+                });
+            });
+        });
+        return promise;
+    }).then(function(){
+        status.success("Successful send notifications to all teachers !!");
+    }, function(error){
+        status.error(error);
+    });
+});
+
 
 /*----------------------------------------------------  CLOUD fUCNTIONs   -----------------------------------------------------*/
 Parse.Cloud.define("getServerTime", function(request, response) {
