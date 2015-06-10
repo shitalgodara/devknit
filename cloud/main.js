fb905cd5ae4371a9e0204ca003befda834824ca2
Shubham Kansal <kansalshubham46@gmail.com>
2015-06-11 00:32:51 +0530

    version  1.12
    Added Background Jobs for likes and comments

--- cloud/main.js
+++ cloud/main.js
@@ -161,30 +161,6 @@ myObject.set("status","LEAVE");
 });
     
 /*---------------------------------------------JOBS----------------------*/
-Parse.Cloud.job("userMigration", function(request, status) {
-    // Set up to modify user data
-    Parse.Cloud.useMasterKey();
-    var counter = 0;
-    // Query for all users
-    var query = new Parse.Query("Codegroup");
-    query.each(function(user) {
-        // Update to plan value passed in
-        user.set("plan", request.params.plan);
-        if (counter % 100 === 0) {
-            // Set the  job's progress status
-            status.message(counter + " users processed.");
-        }
-        counter += 1;
-        return user.save();
-    }).then(function() {
-        // Set the job's success status
-        status.success("Migration completed successfully.");
-    }, function(error) {
-        // Set the job's error status
-        status.error("Uh oh, something went wrong.");
-    });
-});
-
 Parse.Cloud.job("sendLikeNotifications", function(request, status){ 
     var intervals = 4; 
     var intervalTime = (86400000/ intervals);
@@ -195,36 +171,81 @@ Parse.Cloud.job("sendLikeNotifications", function(request, status){
     var query1 = new Parse.Query("GroupDetails");
     query1.greaterThanOrEqualTo("createdAt", dateLowerBound);
     query1.lessThan("createdAt", dateUpperBound);
-    query1.greaterThanOrEqualTo("like_count", 0);
+    query1.greaterThan("like_count", 0);
     query1.select("objectId", "name", "title", "like_count", "senderId");
     query1.find().then(function(results){
         var promise = Parse.Promise.as();
-        Parse.Cloud.useMasterKey();
         _.each(results, function(result){
+            Parse.Cloud.useMasterKey();
             var query2 = new Parse.Query(Parse.Installation);
-            query2.equalTo("username", result.get("senderId")); 
+            var username = result.get("senderId");
+            var msg = result.get("like_count") + " people like your post";
+            console.log(username + ": " + msg);
+            query2.equalTo("username", username); 
             promise = promise.then(function(){
                 return Parse.Push.send({
-                    where: query2,
-                    data: {
-                        msg: "Hey !:)",
-                        alert: "Hey! :)",
-                        badge: "Increment",
-                        groupName: "classname",
-                        type: "NORMAL",
-                action: "INBOX"
-                    }
+                  where: query2, 
+                  data: {
+                    msg: msg,
+                    alert: msg,
+                    badge: "Increment",
+                    groupName: result.get("name"),
+                    type: "TRANSITION",
+                    action: "OUTBOX"
+                  }
                 });
             });
         });
         return promise;
     }).then(function(){
-        status.success("Successful send notifications to all teachers !!");
+        status.success("Successful sent like notifications to all teachers !!");
     }, function(error){
         status.error(error);
     });
 });
 
+Parse.Cloud.job("sendConfusedNotifications", function(request, status){ 
+    var intervals = 4; 
+    var intervalTime = (86400000/ intervals);
+    var date = new Date();
+    var currentTime = date.getTime();
+    var dateLowerBound = new Date(currentTime - intervals * intervalTime);
+    var dateUpperBound = new Date(currentTime - (intervals - 1) * intervalTime);
+    var query1 = new Parse.Query("GroupDetails");
+    query1.greaterThanOrEqualTo("createdAt", dateLowerBound);
+    query1.lessThan("createdAt", dateUpperBound);
+    query1.greaterThan("confused_count", 0);
+    query1.select("objectId", "name", "title", "confused_count", "senderId");
+    query1.find().then(function(results){
+        var promise = Parse.Promise.as();
+        _.each(results, function(result){
+            Parse.Cloud.useMasterKey();
+            var query2 = new Parse.Query(Parse.Installation);
+            var username = result.get("senderId");
+            var msg = result.get("confused_count") + " people seems to be confused by your post";
+            console.log(username + ": " + msg);
+            query2.equalTo("username", username); 
+            promise = promise.then(function(){
+                return Parse.Push.send({
+                  where: query2, 
+                  data: {
+                    msg: msg,
+                    alert: msg,
+                    badge: "Increment",
+                    groupName: result.get("name"),
+                    type: "TRANSITION",
+                    action: "OUTBOX"
+                  }
+                });
+            });
+        });
+        return promise;
+    }).then(function(){
+        status.success("Successful sent confused notifications to all teachers !!");
+    }, function(error){
+        status.error(error);
+    });
+});
 
 /*----------------------------------------------------  CLOUD fUCNTIONs   -----------------------------------------------------*/
 Parse.Cloud.define("getServerTime", function(request, response) {
