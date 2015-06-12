573b4da5b6656c6d81f6fc305e88627075a97c9b
shitalgodara <shital@trumplab.com>
2015-06-12 20:08:36 +0530

    Revert "version 1.15"
    This reverts commit eb5b8136a8ad1bd19d8b9ccd3fd09a06384d306f.

--- cloud/time.js
+++ cloud/time.js
@@ -1,57 +1,40 @@
-/*
-Function to get timedetails of the class corresponding to classcode
-  Input =>
-    classcode: String
-    limit: Number
-  Output =>
-    Created Timedetails of messages of that class
-  Procedure =>
-    A simple query on GroupDetails 
-*/
-exports.toupdatetimebyclass = function(request, response){
-  var clcode = request.params.classcode;
-  var limit = request.params.limit;
-  var GroupDetails = Parse.Object.extend("GroupDetails");
-  var query = new Parse.Query("GroupDetails");
-  query.equalTo("code", clcode);
-  query.select("createdAt");
-  query.descending("createdAt");
-  query.limit(limit);
-  query.find({
-    success: function(results){
-      response.success(results);
-    },
-    error: function(error){
-      response.error("Error: " + error.code + " " + error.message);
-    }
-  });
+//function-4
+//input classcode and limit output timedetails of messages of that class
+exports.toupdatetimebyclass = function(request, response) {
+    var clcode = request.params.classcode;
+    var limit = request.params.limit;
+    var GroupDetails = Parse.Object.extend("GroupDetails");
+    var query = new Parse.Query("GroupDetails");
+    query.equalTo("code", clcode);
+    query.select("createdAt");
+    query.descending("createdAt");
+    query.limit(limit);
+    query.find({
+        success: function(results) {
+            response.success(results);
+        },
+        error: function(error) {
+            response.error("Error: " + error.code + " " + error.message);
+        }
+    });
 }
-
-/*
-Function to get timedetails of the classes
-  Input =>
-    classarray: Array of class codes
-    limit: Number
-  Output =>
-    Created Timedetails of messages of that class
-  Procedure =>
-    A simple query on GroupDetails 
-*/
-exports.toupdatetime = function(request, response){
-  var clarray = request.params.classarray;
-  var limit = request.params.limit;
-  var GroupDetails = Parse.Object.extend("GroupDetails");
-  var query = new Parse.Query("GroupDetails");
-  query.containedIn("code", clarray);
-  query.select("createdAt");
-  query.descending("createdAt");
-  query.limit(limit);
-  query.find({
-    success: function(results){
-      response.success(results);
-    },
-    error: function(error){
-      response.error("Error: " + error.code + " " + error.message);
-    }
-  });
+//function-5
+//input classcodearray and limit output timedetails of messages of that class
+exports.toupdatetime = function(request, response) {
+    var clarray = request.params.classarray;
+    var limit = request.params.limit;
+    var GroupDetails = Parse.Object.extend("GroupDetails");
+    var query = new Parse.Query("GroupDetails");
+    query.containedIn("code", clarray);
+    query.select("createdAt");
+    query.descending("createdAt");
+    query.limit(limit);
+    query.find({
+        success: function(results) {
+            response.success(results);
+        },
+        error: function(error) {
+            response.error("Error: " + error.code + " " + error.message);
+        }
+    });
 }
